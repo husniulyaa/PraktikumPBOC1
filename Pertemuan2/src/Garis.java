4c115e89d2ae4080c6e0cb67bf82853cee16c8e0
husni ulyaa k <husniulyaa11@gmail.com>
2026-03-01 14:03:42 +0700

    Pertemuan 2 - Garis

--- Pertemuan2/src/Garis.java
+++ Pertemuan2/src/Garis.java
@@ -6,26 +6,101 @@
 
 public class Garis {
     /***************ATRIBUT***************/
-    Titik a;
-    Titik b;
+    Titik T1;
+    Titik T2;
     static int counterGaris = 0;
 
     /***************METHOD***************/
     // konstruktor untuk membuat garis
     Garis(){ 
-        a = new Titik();
-        b = new Titik(1, 1);
+        T1 = new Titik();
+        T2 = new Titik(1, 1);
         counterGaris++;
     }
 
     Garis(double x1, double y1, double x2, double y2){
-        this.a = new Titik(x1, y1);
-        this.b = new Titik(x2, y2);
+        this.T1 = new Titik(x1, y1);
+        this.T2 = new Titik(x2, y2);
         counterGaris++;
     }
 
-    Titik geTitik(){
-        return this.a;
+    Titik geTitikAwal(){
+        return this.T1;
+    }
+
+    Titik geTitikAkhir(){
+        return this.T2;
+    }
+
+    void seTitikAwal(Titik T){
+        T1.setAbsis(T.getAbsis());
+        T1.setOrdinat(T.getOrdinat());
+    }
+
+    void seTitikAkhir(Titik T){
+        T2.setAbsis(T.getAbsis());
+        T2.setOrdinat(T.getOrdinat());
+    }
+
+    static int getCounterGaris(){
+        return counterGaris;
+    }
+
+    double panjangGaris(){
+        return Math.sqrt(Math.pow(T1.getAbsis() - T2.getAbsis(), 2)+Math.pow(T1.getOrdinat()-T2.getOrdinat(), 2));
+    }
+
+    double gradien(){
+        return (T2.getOrdinat()-T1.getOrdinat())/(T2.getAbsis()-T2.getAbsis());
+    }
+
+    Titik titikTengah(){
+        Titik T = new Titik();
+        T.setAbsis((T1.getAbsis()+T2.getAbsis())/2);
+        T.setOrdinat((T1.getOrdinat()+T2.getOrdinat())/2);
+
+        return T;
+    }
+
+    boolean isSejajar(Garis G){
+        if (this.gradien() == G.gradien()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    boolean isTegakLurus(Garis G){
+        if (this.gradien() * G.gradien() == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void printGaris(){
+        System.out.println("T. Awal: ("+ T1.getAbsis() + ", " + T1.getOrdinat() + "), T. Akhir: (" + T2.getAbsis() + ", " + T2.getOrdinat() + ")");
+    }
+
+    void persamaanGaris(){
+        // y-y1(x2-x1) = x-x1(y2-y1)
+        // y*x2 - y*x1 - y1*x2 + y1*x1 = y2*x - y1*x - x1*y2 + x1*y1
+        // y*x2 - y*x1 = y2*x - y1*x - x1*y2 + y1*x2
+        // y*x2 - y*x1 = y2*x - y1*x - (x1*y2 - y1*x2)
+        double koefY = T2.getAbsis() - T1.getAbsis();
+        double koefX = (T2.getOrdinat() - T1.getOrdinat());
+        double konst = ((T1.getAbsis() * T2.getOrdinat()) - (T1.getOrdinat() * T2.getAbsis()));
+        
+        if (koefX % koefY == 0){
+            if (konst % koefY == 0){
+                koefX = koefX/koefY;
+                konst = konst/koefY;
+            }
+        }
+        System.out.println("y = " + koefX + "x " + konst);
+
     }
 
 }
