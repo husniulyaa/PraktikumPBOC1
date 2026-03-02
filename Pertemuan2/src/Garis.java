25e7e1a446678acb30508033540c9972e5a701ee
husni ulyaa k <husniulyaa11@gmail.com>
2026-03-02 13:45:02 +0700

    Pertemuan 2

--- Pertemuan2/src/Garis.java
+++ Pertemuan2/src/Garis.java
@@ -6,60 +6,70 @@
 
 public class Garis {
     /***************ATRIBUT***************/
-    Titik T1;
-    Titik T2;
-    static int counterGaris = 0;
+    private Titik T1;
+    private Titik T2;
+    private static int counterGaris = 0;
 
     /***************METHOD***************/
-    // konstruktor untuk membuat garis
-    Garis(){ 
+    // konstruktor untuk membuat garis dengan nilai titik (0, 0) dan (1, 1)
+    public Garis(){ 
         T1 = new Titik();
         T2 = new Titik(1, 1);
         counterGaris++;
     }
 
-    Garis(Titik P1, Titik P2){
+    // konstruktor untuk membuat garis dengan masukan titik 1 dan titik 2
+    public Garis(Titik P1, Titik P2){
         this.T1 = P1;
         this.T2 = P2;
         counterGaris++;
     }
 
-    Titik geTitikAwal(){
+    // selektor untuk mengambil titik awal dari garis
+    public Titik geTitikAwal(){
         return this.T1;
     }
 
-    Titik geTitikAkhir(){
+    // selektor untuk mengambil titik akhir dari garis
+    public Titik geTitikAkhir(){
         return this.T2;
     }
 
-    void seTitikAwal(Titik T){
+    // mutator untuk menge-set titik awal sesuai dengan inputan titik baru
+    public void seTitikAwal(Titik T){
         T1.setAbsis(T.getAbsis());
         T1.setOrdinat(T.getOrdinat());
     }
 
-    void seTitikAkhir(Titik T){
+    // mutator garis untuk set titik akhir dari garis sesuai dengan inputan titik 
+    public void seTitikAkhir(Titik T){
         T2.setAbsis(T.getAbsis());
         T2.setOrdinat(T.getOrdinat());
     }
 
-    void setGaris(Titik p1, Titik p2){
+    // mutator untuk set titik awal dan titik akhir dari suatu garis
+    public void setGaris(Titik p1, Titik p2){
         this.T1 = p1;
         this.T2 = p2;
     }
 
-    static int getCounterGaris(){
+    // fungsi yang akan mengembalikan jumlah banyaknya garis yang sudah dibuat
+    public static int getCounterGaris(){
         return counterGaris;
     }
 
-    double panjangGaris(){
+    // fungsi untuk menghitung panjang garis
+    public double panjangGaris(){
         return Math.sqrt(Math.pow(T1.getAbsis() - T2.getAbsis(), 2)+Math.pow(T1.getOrdinat()-T2.getOrdinat(), 2));
     }
 
-    double gradien(){
+    // fungsi untuk menghitung nilai gradien/kemiringan dari suatu garis
+    public double gradien(){
         return (T2.getOrdinat()-T1.getOrdinat())/(T2.getAbsis()-T1.getAbsis());
     }
 
-    Titik titikTengah(){
+    // fungsi untuk mendapatkan titik tengah dari suatu garis
+    public Titik titikTengah(){
         Titik T = new Titik();
         T.setAbsis((T1.getAbsis()+T2.getAbsis())/2);
         T.setOrdinat((T1.getOrdinat()+T2.getOrdinat())/2);
@@ -67,7 +77,8 @@ public class Garis {
         return T;
     }
 
-    boolean isSejajar(Garis G){
+    // fungsi yang mengembalikan nilai true jika garis sejajar dan false jika tidak sejajar
+    public boolean isSejajar(Garis G){
         if (this.gradien() == G.gradien()){
             return true;
         }
@@ -76,7 +87,8 @@ public class Garis {
         }
     }
 
-    boolean isTegakLurus(Garis G){
+    // fungsi yang mengembalikan nilai true jika garis tegak lurus dan false jika tidak tegak lurus
+    public boolean isTegakLurus(Garis G){
         if (this.gradien() * G.gradien() == -1){
             return true;
         }
@@ -85,11 +97,13 @@ public class Garis {
         }
     }
 
-    void printGaris(){
+    // mengeprint koordinat garis mulai dari titik awal dan titik akhir
+    public void printGaris(){
         System.out.println("T. Awal: ("+ T1.getAbsis() + ", " + T1.getOrdinat() + "), T. Akhir: (" + T2.getAbsis() + ", " + T2.getOrdinat() + ")");
     }
 
-    void persamaanGaris(){
+    // mengembalikan persamaan dari suatu garis
+    public void persamaanGaris(){
         // y-y1(x2-x1) = x-x1(y2-y1)
         // y*x2 - y*x1 - y1*x2 + y1*x1 = y2*x - y1*x - x1*y2 + x1*y1
         // y*x2 - y*x1 = y2*x - y1*x - x1*y2 + y1*x2
--- Pertemuan2/src/Titik.java
+++ Pertemuan2/src/Titik.java
@@ -6,62 +6,62 @@
 
 public class Titik {
     /***************ATRIBUT***************/
-    double absis;
-    double ordinat;
-    static int counterTitik = 0;
+    private double absis;
+    private double ordinat;
+    private static int counterTitik = 0;
 
     /***************METHOD***************/
     //konstruktor untuk membuat titik (0,0)
-    Titik(){
+    public Titik(){
         this.absis = 0;
         this.ordinat = 0;
         counterTitik++;
     }
 
     //konstruktor untuk membuat titik (x, y)
-    Titik(double x, double y){
+    public Titik(double x, double y){
         this.absis = x;
         this.ordinat = y;
     }
 
     //mengembalikan nilai absis
-    double getAbsis(){
+    public double getAbsis(){
         return this.absis;
     }
 
     //mengembalikan nilai ordinat
-    double getOrdinat(){
+    public double getOrdinat(){
         return this.ordinat;
     }
 
     //mengubah nilai absis
-    void setAbsis(double x){
+    public void setAbsis(double x){
         this.absis = x;
     }
 
     //mengubah nilai ordinat
-    void setOrdinat(double y){
+    public void setOrdinat(double y){
         this.ordinat = y;
     }
 
     //menggeser absis sejauh x dan ordinat sejauh y
-    void geser(double x, double y){
+    public void geser(double x, double y){
         absis = this.absis + x;
         ordinat = this.ordinat + y;
     }
 
     //mencetak koordinat titik
-    void prinTitik(){
+    public void prinTitik(){
         System.out.println("Titik (" + absis + ", " + ordinat + ")");
     }
 
     // mencetak banyak titik yang sudah dibuat
-    static int getCounterTitik(){
+    public static int getCounterTitik(){
         return counterTitik;
     }
 
     // mencetak nilai kuadran dari suatu titik
-    int getKuadran(){
+    public int getKuadran(){
         if (this.absis == 0 && this.ordinat == 0){
             return -1;
         }
@@ -88,27 +88,27 @@ public class Titik {
     }
 
     // mencetak jarak antara titik dengan titik pusat (0,0)
-    double getJarakPusat(){
+    public double getJarakPusat(){
         return Math.sqrt(Math.pow(this.absis, 2) + Math.pow(this.ordinat, 2));
     }
 
     // mencetak jarak antara titik dengan titik T (x,y)
-    double getJarak(Titik T){
+    public double getJarak(Titik T){
         return Math.sqrt(Math.pow(this.absis - T.getAbsis(), 2) + Math.pow(this.ordinat - T.getOrdinat(), 2));
     }
 
     // melakukan refleksi terhadap sumbu x pada titik 
-    void refleksiX(){
+    public void refleksiX(){
         setOrdinat(this.ordinat*(-1)); 
     }
 
     // melakukan refleksi terhadap sumbu y pada titik
-    void refleksiY(){
+    public void refleksiY(){
         setAbsis(this.absis*(-1));
     }
 
     // mencetak titik baru hasil refleksi dari titik terhadap sumbu x
-    Titik getRefleksiX(){
+    public Titik getRefleksiX(){
         Titik Tnew = new Titik();
         Tnew.setAbsis(this.absis);
         Tnew.setOrdinat(this.ordinat*(-1));
@@ -117,7 +117,7 @@ public class Titik {
     }
 
     // mencetak titik baru hasil refleksi dari titik terhadap sumbu y
-    Titik getRefleksiY(){
+    public Titik getRefleksiY(){
         Titik Tnew = new Titik();
         Tnew.setAbsis(this.absis*(-1));
         Tnew.setOrdinat(this.ordinat);
